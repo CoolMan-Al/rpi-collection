e7484594d3ff5bbdf06b07573e6f1047b52b84d6
CoolMan-Al <168761899+CoolMan-Al@users.noreply.github.com>
2024-05-13 14:01:21 +0100

    add new script

---
+++ reaction_game.py
@@ -0,0 +1,25 @@
+from gpiozero import LED, Button
+from time import sleep
+from random import uniform
+
+led = LED(26)
+l_btn = Button(21)
+r_btn = Button(20)
+
+count = 0
+
+sleep(uniform(5,10))
+led.on()
+
+while True:
+    count+= 1
+    sleep(0.001)
+
+    if l_btn.is_active == True or r_btn.is_active == True:
+        if l_btn.is_active == True:
+            print("Left Side Won!")
+        elif r_btn.is_active == True:
+            print("Right Side Won!")
+        print("You took " + str(count) + "ms.")
+        finished = True
+        quit()
