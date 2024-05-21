98458aec18dfab132d1100da37a08ea19d1cd95c
Al <168761899+CoolMan-Al@users.noreply.github.com>
2024-05-21 11:58:57 +0100

    Fixed direction issues

--- sense_hat_scripts/hat_movement.py
+++ sense_hat_scripts/hat_movement.py
@@ -31,51 +31,51 @@ def up(event):
         global row
         global col
         if row == 0:
-            sense.set_pixel(row,col,100,0,0)
+            sense.set_pixel(col,row,100,0,0)
         
         else:
             row -= 1
-            sense.set_pixel(row,col,color)
+            sense.set_pixel(col,row,color)
 
 def down(event):
     if event.action == 'pressed':
         global row
         global col
         if row == 7:
-            sense.set_pixel(row,col,100,0,0)
+            sense.set_pixel(col,row,100,0,0)
         
         else:
             row += 1
-            sense.set_pixel(row,col,color)
+            sense.set_pixel(col,row,color)
 
 def left(event):
     if event.action == 'pressed':
         global row
         global col
         if col == 0:
-            sense.set_pixel(row,col,100,0,0)
+            sense.set_pixel(col,row,100,0,0)
         
         else:
             col -= 1
-            sense.set_pixel(row,col,color)
+            sense.set_pixel(col,row,color)
 
 def right(event):
     if event.action == 'pressed':
         global row
         global col
         if col == 7:
-            sense.set_pixel(row,col,100,0,0)
+            sense.set_pixel(col,row,100,0,0)
         
         else:
             col += 1
-            sense.set_pixel(row,col,color)
+            sense.set_pixel(col,row,color)
 
 sense.set_pixel(0,0, color)
 
-sense.stick.direction_left   = up
-sense.stick.direction_right  = down
-sense.stick.direction_up     = left
-sense.stick.direction_down   = right
+sense.stick.direction_up     = up
+sense.stick.direction_down   = down
+sense.stick.direction_left   = left
+sense.stick.direction_right  = right
 sense.stick.direction_middle = change
 
 while True:
