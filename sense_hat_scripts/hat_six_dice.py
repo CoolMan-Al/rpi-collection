785c8e0a2ac4e50dc9c6dd2be05621b8580aba95
Al <168761899+CoolMan-Al@users.noreply.github.com>
2024-06-07 17:06:22 +0100

    Update hat_six_dice.py

--- sense_hat_scripts/hat_six_dice.py
+++ sense_hat_scripts/hat_six_dice.py
@@ -2,11 +2,16 @@ from random import randint
 from sense_hat import SenseHat
 from time import sleep
 
+def roll():
+    sense.set_pixels(dice[randint(0,5)])
+    sleep(3)
+    sense.clear()
+
 sense = SenseHat()
 sense.clear()
 
 blk = [0,0,0]
-pix = [100,100,100]
+pix = [randint(0,100),randint(0,100),randint(0,100)]
 
 dice = [
     [ # One
@@ -71,12 +76,12 @@ dice = [
     ]
 ]
 
+sense.stick.direction_any = roll
+
 while True:
     gyro = sense.get_accelerometer_raw()
     for i in gyro:
         gyro[i] = round(gyro[i])
     
     if gyro['x'] != 0 or gyro['y'] != 0 or gyro['z'] != 1:
-        sense.set_pixels(dice[randint(0,5)])
-        sleep(3)
-        sense.clear()
+        roll()
