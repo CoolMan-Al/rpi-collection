70c19a562ad30e0e6de251892d4d9707080859c9
Al <168761899+CoolMan-Al@users.noreply.github.com>
2024-06-07 17:07:01 +0100

    Update hat_twenty_dice.py

--- sense_hat_scripts/hat_twenty_dice.py
+++ sense_hat_scripts/hat_twenty_dice.py
@@ -2,10 +2,17 @@ from random import randint
 from sense_hat import SenseHat
 from time import sleep
 
+def roll():
+    sense.show_message(str(randint(0,20)), text_colour=[randint(0,100),randint(0,100),randint(0,100)])
+    sense.clear()
+    sleep(3)
+
 sense = SenseHat()
 sense.clear()
 
 pix = [randint(0,100),randint(0,100),randint(0,100)]
+
+sense.stick.direction_any = roll
 
 while True:
     gyro = sense.get_accelerometer_raw()
@@ -13,5 +20,4 @@ while True:
         gyro[i] = round(gyro[i])
     
     if gyro['x'] != 0 or gyro['y'] != 0 or gyro['z'] != 1:
-        sense.show_message(str(randint(0,20)), text_colour=[randint(0,100),randint(0,100),randint(0,100)])
-        sense.clear()
+        roll()
